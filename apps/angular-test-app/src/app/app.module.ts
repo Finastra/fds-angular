ff16adc2122ad4cb6c7ad3a702554a3eeab5c7d9
Talbot, Thomas <thomas.talbot@finastra.com>
2023-03-29 13:04:30 +0200

    bringing some components back to test-app

--- apps/angular-test-app/src/app/app.module.ts
+++ apps/angular-test-app/src/app/app.module.ts
@@ -12,57 +12,57 @@ import { MaterialModule } from './material.module';
 // import { RepeaterCardChartExampleComponent } from './components/repeater-demo/repeater-card-chart-example/repeater-card-chart-example.component';
 import { AvatarDemoComponent } from './components/avatar-demo/avatar-demo.component';
 // import { ChartDemoComponent } from './components/chart-demo/chart-demo.component';
-// import { ExpandableTableDemoComponent } from './components/expandable-table-demo/expandable-table-demo.component';
-// import { FilterPanelDemoComponent } from './components/filter-panel-demo/filter-panel-demo.component';
+import { ExpandableTableDemoComponent } from './components/expandable-table-demo/expandable-table-demo.component';
+import { FilterPanelDemoComponent } from './components/filter-panel-demo/filter-panel-demo.component';
 import { FoundationsDemoComponent } from './components/foundations-demo/foundations-demo.component';
 // import { GlobalSearchDemoComponent } from './components/global-search-demo/global-search-demo.component';
 import { HomeComponent } from './components/home/home.component';
-// import { PopoverDemoComponent } from './components/popover-demo/popover-demo.component';
+import { PopoverDemoComponent } from './components/popover-demo/popover-demo.component';
 // import { RepeaterDemoComponent } from './components/repeater-demo/repeater-demo.component';
 // import { FieldMatcherComponent } from './components/repeater-demo/field-matcher/field-matcher.component';
-// import { SkeletonDemoComponent } from './components/skeleton-demo/skeleton-demo.component';
+import { SkeletonDemoComponent } from './components/skeleton-demo/skeleton-demo.component';
 // import { TableDemoComponent } from './components/table-demo/table-demo.component';
 // import { ThemeBuilderComponent } from './components/theme-builder/theme-builder-demo.component';
 import { UiElementsDemoComponent } from './components/ui-elements-demo/ui-elements-demo.component';
-// import { UserProfileMenuDemoComponentComponent } from './components/user-profile-menu-demo/user-profile-menu-demo-component.component';
+import { UserProfileMenuDemoComponentComponent } from './components/user-profile-menu-demo/user-profile-menu-demo-component.component';
 // import { VectorMapDemoComponent } from './components/vector-map-demo/vector-map-demo.component';
 // import { WizardDemoComponent } from './components/wizard-demo/wizard-demo.component';
-// import { BannerDemoComponent } from './components/banner-demo/banner-demo.component';
-// import { EntityMenuDemoComponent } from './components/entity-menu-demo/entity-menu-demo.component';
+import { BannerDemoComponent } from './components/banner-demo/banner-demo.component';
+import { EntityMenuDemoComponent } from './components/entity-menu-demo/entity-menu-demo.component';
 // import { ChangelogDemoComponent } from './components/changelog-demo/changelog-demo.component';
-// import { SearchInputDemoComponent } from './components/search-input-demo/search-input-demo.component';
-// import { BreadcrumbDemoComponent } from './components/breadcrumb-demo/breadcrumb-demo.component';
+import { BreadcrumbDemoComponent } from './components/breadcrumb-demo/breadcrumb-demo.component';
+import { SearchInputDemoComponent } from './components/search-input-demo/search-input-demo.component';
 // import { ToasterDemoComponent } from './components/toaster-demo/toaster-demo.component';
 
 // import { GlobalSearchModule } from '@finastra/angular-components/global-search';
 // import { RepeaterModule } from '@finastra/angular-components/repeater';
 // import { ScrollToTopModule } from '@finastra/angular-components/scroll-to-top';
 // import { UxgTableModule } from '@finastra/angular-components/table';
-// import { PopoverModule } from '@finastra/angular-components/popover';
-// import { FilterTreeModule } from '@finastra/angular-components/filter/filter-tree';
-// import { FilterTagsModule } from '@finastra/angular-components/filter/filter-tags';
+import { FilterTagsModule } from '@finastra/angular-components/filter/filter-tags';
+import { FilterTreeModule } from '@finastra/angular-components/filter/filter-tree';
+import { PopoverModule } from '@finastra/angular-components/popover';
 // import { WizardModule } from '@finastra/angular-components/wizard';
-// import { FilterGroupModule } from '@finastra/angular-components/filter/filter-group';
-// import { FilterToggleModule } from '@finastra/angular-components/filter/filter-toggle';
-// import { MultiselectTagsModule } from '@finastra/angular-components/filter/multiselect-tags';
+import { FilterGroupModule } from '@finastra/angular-components/filter/filter-group';
+import { FilterToggleModule } from '@finastra/angular-components/filter/filter-toggle';
+import { MultiselectTagsModule } from '@finastra/angular-components/filter/multiselect-tags';
 // import { VectorMapModule } from '@finastra/angular-components/vector-map';
 // import { ChartModule } from '@finastra/angular-components/chart';
 import { AvatarModule } from '@finastra/angular-components/avatar';
-// import { ExpandableTableModule } from '@finastra/angular-components/expandable-table';
+import { ExpandableTableModule } from '@finastra/angular-components/expandable-table';
 // import { PaletteModule } from '@finastra/angular-components/core';
-// import { SkeletonTextModule } from '@finastra/angular-components/skeleton-text';
-// import { UxgUserProfileMenuModule } from '@finastra/angular-components/user-profile-menu';
-// import { BannerModule } from '@finastra/angular-components/banner';
+import { BannerModule } from '@finastra/angular-components/banner';
+import { SkeletonTextModule } from '@finastra/angular-components/skeleton-text';
+import { UxgUserProfileMenuModule } from '@finastra/angular-components/user-profile-menu';
 // import { GlobalNavModule } from '@finastra/angular-components/global-nav';
 // import { GlobalNavDemoComponent } from './components/global-nav-demo/global-nav-demo.component';
-// import { AccountCardDemoComponent } from './components/account-card-demo/account-card-demo.component';
-// import { AccountCardModule } from '@finastra/angular-components/cards/account-card';
-// import { EntityMenuModule } from '@finastra/angular-components/entity-menu';
+import { AccountCardModule } from '@finastra/angular-components/cards/account-card';
+import { EntityMenuModule } from '@finastra/angular-components/entity-menu';
+import { AccountCardDemoComponent } from './components/account-card-demo/account-card-demo.component';
 // import { ColorSketchModule } from 'ngx-color/sketch';
 
-// import { UxgSearchInputModule } from '@finastra/angular-components/search-input';
+import { UxgSearchInputModule } from '@finastra/angular-components/search-input';
 // import { UXGChangelogModule } from '@finastra/angular-components/changelog';
-// import { UxgBreadcrumbModule } from '@finastra/angular-components/breadcrumb';
+import { UxgBreadcrumbModule } from '@finastra/angular-components/breadcrumb';
 // import { ToasterModule } from '@finastra/angular-components/toaster';
 // import { VideoThumbnailModule } from '@finastra/angular-components/video-thumbnail';
 // import { VideoThumbnailDemoComponent, VideoDialogComponent } from './components/video-thumbnail/video-thumbnail.component';
@@ -73,36 +73,36 @@ import { AvatarModule } from '@finastra/angular-components/avatar';
   declarations: [
     AvatarDemoComponent,
     // ChartDemoComponent,
-    // ExpandableTableDemoComponent,
-    // FilterPanelDemoComponent,
+    ExpandableTableDemoComponent,
+    FilterPanelDemoComponent,
     FoundationsDemoComponent,
     // GlobalSearchDemoComponent,
     HomeComponent,
-    // PopoverDemoComponent,
+    PopoverDemoComponent,
     // RepeaterDemoComponent,
     // FieldMatcherComponent,
     // RepeaterCardAdvancedExampleComponent,
     // RepeaterCardChartExampleComponent,
     // RepeaterCardExampleComponent,
-    // SkeletonDemoComponent,
+    SkeletonDemoComponent,
     // TableDemoComponent,
     // ThemeBuilderComponent,
     UiElementsDemoComponent,
-    // UserProfileMenuDemoComponentComponent,
+    UserProfileMenuDemoComponentComponent,
     // VectorMapDemoComponent,
     // WizardDemoComponent,
-    AppComponent
-    // BannerDemoComponent,
+    AppComponent,
+    BannerDemoComponent,
     // GlobalNavDemoComponent,
-    // AccountCardDemoComponent,
-    // EntityMenuDemoComponent,
-    // SearchInputDemoComponent,
+    AccountCardDemoComponent,
+    EntityMenuDemoComponent,
+    SearchInputDemoComponent,
     // ChangelogDemoComponent,
-    // BreadcrumbDemoComponent,
+    BreadcrumbDemoComponent,
     // ToasterDemoComponent,
     // VideoThumbnailDemoComponent,
     // VideoDialogComponent,
-    // StatusStepsDemoComponent
+    // StatusStepsDemoComponent,
   ],
   imports: [
     BrowserModule,
@@ -119,29 +119,28 @@ import { AvatarModule } from '@finastra/angular-components/avatar';
     // GlobalSearchModule,
     // PaletteModule,
     // ScrollToTopModule,
-    // PopoverModule,
+    PopoverModule,
     // UxgTableModule,
-    // FilterTagsModule,
-    // FilterTreeModule,
+    FilterTagsModule,
+    FilterTreeModule,
     // VectorMapModule,
     // ChartModule,
     // WizardModule,
-    // FilterGroupModule,
-    // FilterToggleModule,
-    // SkeletonTextModule,
-    // ExpandableTableModule,
-    // MultiselectTagsModule,
-    AvatarModule
-    // UxgUserProfileMenuModule,
-    // BannerModule,
+    FilterGroupModule,
+    FilterToggleModule,
+    SkeletonTextModule,
+    ExpandableTableModule,
+    MultiselectTagsModule,
+    AvatarModule,
+    UxgUserProfileMenuModule,
+    BannerModule,
     // GlobalNavModule,
-    // EntityMenuModule,
+    EntityMenuModule,
     // ColorSketchModule,
-    // AccountCardModule,
-    // EntityMenuModule,
-    // UxgSearchInputModule,
+    AccountCardModule,
+    UxgSearchInputModule,
     // UXGChangelogModule,
-    // UxgBreadcrumbModule,
+    UxgBreadcrumbModule,
     // ToasterModule,
     // VideoThumbnailModule,
     // StatusStepperModule
--- apps/angular-test-app/src/app/nested-routes.ts
+++ apps/angular-test-app/src/app/nested-routes.ts
@@ -1,8 +1,18 @@
 import { Routes } from '@angular/router';
 
+import { AccountCardDemoComponent } from './components/account-card-demo/account-card-demo.component';
 import { AvatarDemoComponent } from './components/avatar-demo/avatar-demo.component';
+import { BannerDemoComponent } from './components/banner-demo/banner-demo.component';
+import { BreadcrumbDemoComponent } from './components/breadcrumb-demo/breadcrumb-demo.component';
+import { EntityMenuDemoComponent } from './components/entity-menu-demo/entity-menu-demo.component';
+import { ExpandableTableDemoComponent } from './components/expandable-table-demo/expandable-table-demo.component';
+import { FilterPanelDemoComponent } from './components/filter-panel-demo/filter-panel-demo.component';
 import { FoundationsDemoComponent } from './components/foundations-demo/foundations-demo.component';
+import { PopoverDemoComponent } from './components/popover-demo/popover-demo.component';
+import { SearchInputDemoComponent } from './components/search-input-demo/search-input-demo.component';
+import { SkeletonDemoComponent } from './components/skeleton-demo/skeleton-demo.component';
 import { UiElementsDemoComponent } from './components/ui-elements-demo/ui-elements-demo.component';
+import { UserProfileMenuDemoComponentComponent } from './components/user-profile-menu-demo/user-profile-menu-demo-component.component';
 
 export const nestedRoutes: { title: string; routes: Routes }[] = [
   {
@@ -17,28 +27,28 @@ export const nestedRoutes: { title: string; routes: Routes }[] = [
     title: 'Components',
     routes: [
       //     { path: 'chart', component: ChartDemoComponent, data: { title: 'Chart' } },
-      //     { path: 'filter-panel', component: FilterPanelDemoComponent, data: { title: 'Filter' } },
+      { path: 'filter-panel', component: FilterPanelDemoComponent, data: { title: 'Filter' } },
       //     { path: 'global-search', component: GlobalSearchDemoComponent, data: { title: 'Global Search' } },
-      //     { path: 'popover', component: PopoverDemoComponent, data: { title: 'Popover' } },
+      { path: 'popover', component: PopoverDemoComponent, data: { title: 'Popover' } },
       //     { path: 'table', component: TableDemoComponent, data: { title: 'Table' } },
       //     { path: 'repeater', component: RepeaterDemoComponent, data: { title: 'Repeater' } },
-      //     { path: 'skeleton', component: SkeletonDemoComponent, data: { title: 'Skeleton' } },
+      { path: 'skeleton', component: SkeletonDemoComponent, data: { title: 'Skeleton' } },
       //     { path: 'vector-map', component: VectorMapDemoComponent, data: { title: 'Vector Map' } },
       //     { path: 'wizard', component: WizardDemoComponent, data: { title: 'Wizard' } },
-      //     { path: 'entity-menu', component: EntityMenuDemoComponent, data: { title: 'Entity Menu' } },
+      { path: 'entity-menu', component: EntityMenuDemoComponent, data: { title: 'Entity Menu' } },
       //     { path: 'global-nav', component: GlobalNavDemoComponent, data: { title: 'Global Nav' } },
-      { path: 'avatar', component: AvatarDemoComponent, data: { title: 'Avatar' } }
-      //     { path: 'expandable-table', component: ExpandableTableDemoComponent, data: { title: 'Expandable Table' } },
-      //     {
-      //       path: 'user-profile-menu',
-      //       component: UserProfileMenuDemoComponentComponent,
-      //       data: { title: 'User Profile Menu' }
-      //     },
-      //     { path: 'banner', component: BannerDemoComponent, data: { title: 'Banner' } },
-      //     { path: 'account-card', component: AccountCardDemoComponent, data: { title: 'Account Card' } },
-      //     { path: 'search-input', component: SearchInputDemoComponent, data: { title: 'Search Input' } },
+      { path: 'avatar', component: AvatarDemoComponent, data: { title: 'Avatar' } },
+      { path: 'expandable-table', component: ExpandableTableDemoComponent, data: { title: 'Expandable Table' } },
+      {
+        path: 'user-profile-menu',
+        component: UserProfileMenuDemoComponentComponent,
+        data: { title: 'User Profile Menu' }
+      },
+      { path: 'banner', component: BannerDemoComponent, data: { title: 'Banner' } },
+      { path: 'account-card', component: AccountCardDemoComponent, data: { title: 'Account Card' } },
+      { path: 'search-input', component: SearchInputDemoComponent, data: { title: 'Search Input' } },
       //     { path: 'changelog', component: ChangelogDemoComponent, data: { title: 'Changelog' } },
-      //     { path: 'breadcrumb', component: BreadcrumbDemoComponent, data: { title: 'Breadcrumb' } },
+      { path: 'breadcrumb', component: BreadcrumbDemoComponent, data: { title: 'Breadcrumb' } },
       //     { path: 'toaster', component: ToasterDemoComponent, data: { title: 'Toaster' } },
       //     { path: 'video-thumbnail', component: VideoThumbnailDemoComponent, data: { title: 'Video Thumbnail' } },
       //     { path: 'status-stepper', component: StatusStepsDemoComponent, data: { title: 'Status Stepper' } }
