32843c62fa1e05d37d7bb26a44ad049aa22f5f0d
Talbot, Thomas <thomas.talbot@finastra.com>
2023-03-30 08:38:30 +0200

    bringing more components

--- apps/angular-test-app/src/app/nested-routes.ts
+++ apps/angular-test-app/src/app/nested-routes.ts
@@ -4,23 +4,33 @@ import { AccountCardDemoComponent } from './components/account-card-demo/account
 import { AvatarDemoComponent } from './components/avatar-demo/avatar-demo.component';
 import { BannerDemoComponent } from './components/banner-demo/banner-demo.component';
 import { BreadcrumbDemoComponent } from './components/breadcrumb-demo/breadcrumb-demo.component';
+import { ChangelogDemoComponent } from './components/changelog-demo/changelog-demo.component';
 import { EntityMenuDemoComponent } from './components/entity-menu-demo/entity-menu-demo.component';
 import { ExpandableTableDemoComponent } from './components/expandable-table-demo/expandable-table-demo.component';
 import { FilterPanelDemoComponent } from './components/filter-panel-demo/filter-panel-demo.component';
 import { FoundationsDemoComponent } from './components/foundations-demo/foundations-demo.component';
+import { GlobalNavDemoComponent } from './components/global-nav-demo/global-nav-demo.component';
+import { GlobalSearchDemoComponent } from './components/global-search-demo/global-search-demo.component';
 import { PopoverDemoComponent } from './components/popover-demo/popover-demo.component';
+import { RepeaterDemoComponent } from './components/repeater-demo/repeater-demo.component';
 import { SearchInputDemoComponent } from './components/search-input-demo/search-input-demo.component';
 import { SkeletonDemoComponent } from './components/skeleton-demo/skeleton-demo.component';
+import { StatusStepsDemoComponent } from './components/status-steps-demo/status-steps-demo.component';
+import { TableDemoComponent } from './components/table-demo/table-demo.component';
+import { ThemeBuilderComponent } from './components/theme-builder/theme-builder-demo.component';
+import { ToasterDemoComponent } from './components/toaster-demo/toaster-demo.component';
 import { UiElementsDemoComponent } from './components/ui-elements-demo/ui-elements-demo.component';
 import { UserProfileMenuDemoComponentComponent } from './components/user-profile-menu-demo/user-profile-menu-demo-component.component';
+import { VideoThumbnailDemoComponent } from './components/video-thumbnail/video-thumbnail.component';
+import { WizardDemoComponent } from './components/wizard-demo/wizard-demo.component';
 
 export const nestedRoutes: { title: string; routes: Routes }[] = [
   {
     title: 'Theme',
     routes: [
       { path: 'foundations', component: FoundationsDemoComponent, data: { title: 'Foundations' } },
-      { path: 'ui-elements', component: UiElementsDemoComponent, data: { title: 'UI Elements' } }
-      // { path: 'theme-builder', component: ThemeBuilderComponent, data: { title: 'Theme Builder' } }
+      { path: 'ui-elements', component: UiElementsDemoComponent, data: { title: 'UI Elements' } },
+      { path: 'theme-builder', component: ThemeBuilderComponent, data: { title: 'Theme Builder' } }
     ]
   },
   {
@@ -28,15 +38,15 @@ export const nestedRoutes: { title: string; routes: Routes }[] = [
     routes: [
       //     { path: 'chart', component: ChartDemoComponent, data: { title: 'Chart' } },
       { path: 'filter-panel', component: FilterPanelDemoComponent, data: { title: 'Filter' } },
-      //     { path: 'global-search', component: GlobalSearchDemoComponent, data: { title: 'Global Search' } },
+      { path: 'global-search', component: GlobalSearchDemoComponent, data: { title: 'Global Search' } },
       { path: 'popover', component: PopoverDemoComponent, data: { title: 'Popover' } },
-      //     { path: 'table', component: TableDemoComponent, data: { title: 'Table' } },
-      //     { path: 'repeater', component: RepeaterDemoComponent, data: { title: 'Repeater' } },
+      { path: 'table', component: TableDemoComponent, data: { title: 'Table' } },
+      { path: 'repeater', component: RepeaterDemoComponent, data: { title: 'Repeater' } },
       { path: 'skeleton', component: SkeletonDemoComponent, data: { title: 'Skeleton' } },
       //     { path: 'vector-map', component: VectorMapDemoComponent, data: { title: 'Vector Map' } },
-      //     { path: 'wizard', component: WizardDemoComponent, data: { title: 'Wizard' } },
+      { path: 'wizard', component: WizardDemoComponent, data: { title: 'Wizard' } },
       { path: 'entity-menu', component: EntityMenuDemoComponent, data: { title: 'Entity Menu' } },
-      //     { path: 'global-nav', component: GlobalNavDemoComponent, data: { title: 'Global Nav' } },
+      { path: 'global-nav', component: GlobalNavDemoComponent, data: { title: 'Global Nav' } },
       { path: 'avatar', component: AvatarDemoComponent, data: { title: 'Avatar' } },
       { path: 'expandable-table', component: ExpandableTableDemoComponent, data: { title: 'Expandable Table' } },
       {
@@ -47,11 +57,11 @@ export const nestedRoutes: { title: string; routes: Routes }[] = [
       { path: 'banner', component: BannerDemoComponent, data: { title: 'Banner' } },
       { path: 'account-card', component: AccountCardDemoComponent, data: { title: 'Account Card' } },
       { path: 'search-input', component: SearchInputDemoComponent, data: { title: 'Search Input' } },
-      //     { path: 'changelog', component: ChangelogDemoComponent, data: { title: 'Changelog' } },
-      { path: 'breadcrumb', component: BreadcrumbDemoComponent, data: { title: 'Breadcrumb' } }
-      //     { path: 'toaster', component: ToasterDemoComponent, data: { title: 'Toaster' } },
-      //     { path: 'video-thumbnail', component: VideoThumbnailDemoComponent, data: { title: 'Video Thumbnail' } },
-      //     { path: 'status-stepper', component: StatusStepsDemoComponent, data: { title: 'Status Stepper' } }
+      { path: 'changelog', component: ChangelogDemoComponent, data: { title: 'Changelog' } },
+      { path: 'breadcrumb', component: BreadcrumbDemoComponent, data: { title: 'Breadcrumb' } },
+      { path: 'toaster', component: ToasterDemoComponent, data: { title: 'Toaster' } },
+      { path: 'video-thumbnail', component: VideoThumbnailDemoComponent, data: { title: 'Video Thumbnail' } },
+      { path: 'status-stepper', component: StatusStepsDemoComponent, data: { title: 'Status Stepper' } }
     ]
   }
 ].map((groups) => {
