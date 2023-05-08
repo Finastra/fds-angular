f916dc4f686cba560e768170992c5c6d703f13e7
Razvan Bordeanu <razvan.bordeanu@finastra.com>
2023-05-08 16:09:23 +0300

    tweaks + add new list demo

---
+++ apps/angular-test-app/src/app/components/list-demo/list-demo.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+
+@Component({
+  selector: 'ffdc-list-demo',
+  templateUrl: './list-demo.component.html',
+  styleUrls: ['./list-demo.component.scss'],
+  standalone: true,
+  imports: [MatListModule, MatIconModule]
+})
+export class ListDemoComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
